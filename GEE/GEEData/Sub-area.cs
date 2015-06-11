6402b2c86ddca04fd3ffaf1886222493705d55fb
Joao Pedro P. da Silva <joaopedro-ps@hotmail.com>
2015-06-10 21:32:35 -0300

    Mais Classes
    Area e Sub area

---
+++ GEE/GEEData/Sub-area.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEEData
+{
+   public class Sub_area
+    {
+        public int id { get; set; }
+        public string subArea { get; set; }
+    }
+}
