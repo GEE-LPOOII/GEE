afa0c50b0a68ddef6d0b235e6ff723cf1635ab50
Joao Pedro P. da Silva <joaopedro-ps@hotmail.com>
2015-06-10 21:26:25 -0300

    Classes
    Classes e atributos

---
+++ GEE/GEEData/Eventos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEEData
+{
+   public class Eventos
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime data { get; set; }
+        public string cidade { get; set; }
+        public int qtd_horas { get; set; }
+        public string descricao { get; set; }
+    }
+}
